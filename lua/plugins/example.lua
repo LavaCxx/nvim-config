20f735f960147b85b2856e66788b29c9f5353cfa
lavac <lavac@foxmail.com>
2023-09-21 09:48:49 +0800

    merge

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -95,6 +95,18 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        eslint = {}
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
   },
