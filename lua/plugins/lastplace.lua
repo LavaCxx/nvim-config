51d7b09dea9cfc3d43dbd2c2db91a4911378cf1e
lavac <lavac@foxmail.com>
2023-09-22 10:34:43 +0800

    add some plugins

---
+++ lua/plugins/lastplace.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "ethanholz/nvim-lastplace",
+        lazy = true,
+        event = { "User FileOpened" },
+        config = function()
+            require("nvim-lastplace").setup({
+                lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+                lastplace_ignore_filetype = {
+                    "gitcommit",
+                    "gitrebase",
+                    "svn",
+                    "hgcommit",
+                },
+                lastplace_open_folds = true,
+            })
+        end,
+    }
+}
